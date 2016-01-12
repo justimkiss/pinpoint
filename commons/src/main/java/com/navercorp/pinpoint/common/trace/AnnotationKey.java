c4d5fdccb013be4c3bc0e1b70c17ac4bb69feb3e
Jinkai.Ma <majinkai@handu.com>
2016-01-12 16:53:18 +0800

    Add support RabbitMQ publish messages

--- commons/src/main/java/com/navercorp/pinpoint/common/trace/AnnotationKey.java
+++ commons/src/main/java/com/navercorp/pinpoint/common/trace/AnnotationKey.java
@@ -81,6 +81,10 @@ import static com.navercorp.pinpoint.common.trace.AnnotationKeyProperty.*;
  * <tr><td>82</td><td>thrift.result</td></tr>
  * <tr><td>90</td><td>dubbo.args</td></tr>
  * <tr><td>91</td><td>dubbo.result</td></tr>
+ * <tr><td>100</td><td>rabbitmq.properties</td></tr>
+ * <tr><td>101</td><td>rabbitmq.body</td></tr>
+ * <tr><td>102</td><td>rabbitmq.properties</td></tr>
+ * <tr><td>103</td><td>rabbitmq.body</td></tr>
  * <tr><td>9000</td><td>gson.json.length</td></tr>
  * <tr><td>9001</td><td>jackson.json.length</td></tr>
  * <tr><td>9002</td><td>json-lib.json.length</td></tr>
